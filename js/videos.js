e8c9c39920b180948b63810d5007186ea55ed19c
Derek <derekisinternet@gmail.com>
2018-04-19 22:02:57 -0700

    added buttons that link to previous videos

--- js/videos.js
+++ js/videos.js
@@ -1,4 +1,7 @@
 // Select which video to play on the main player:
 function setVideo(link) {
   document.getElementById('youtube-player').src = link;
+  //redraw the player:
+  document.getElementById('youtube-player').parentElement.style.display = 'none';
+  document.getElementById('youtube-player').parentElement.style.display = 'block';
 }
