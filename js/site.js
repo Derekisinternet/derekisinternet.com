0651233bb6775abe2951dcf7b5e1d698684c6fb7
Derek <derek.aubert@concur.com>
2019-08-21 12:45:06 -0700

    added files

---
+++ js/site.js
@@ -0,0 +1,8 @@
+function initNavigator(element) {
+  console.log("this thing on?");
+  req = new XMLHttpRequest();
+  req.open('GET', "/navigator.html", false);
+  console.log(req.responseText);
+  req.send(null);
+  element.innerHTML = req.responseText;
+}
