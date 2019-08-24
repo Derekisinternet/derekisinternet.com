64a8460c6ab2d40814a6b6e3caa82b23d1484c22
Derek <derekisinternet@gmail.com>
2019-08-23 20:04:15 -0700

    tweaked css

--- js/site.js
+++ js/site.js
@@ -1,7 +1,7 @@
 function initNavigator(element) {
   console.log("this thing on?");
   req = new XMLHttpRequest();
-  req.open('GET', "/navigator.html", false);
+  req.open('GET', "navigator.html", false);
   console.log(req.responseText);
   req.send(null);
   element.innerHTML = req.responseText;
