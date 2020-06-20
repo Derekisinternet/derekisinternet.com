8e50619594ecd404a7b56c8f5147e7255ac04330
Derek <derek.aubert@concur.com>
2020-06-20 12:16:21 -0700

    starting accordion menu

--- demos/js/color_picker.js
+++ demos/js/color_picker.js
@@ -1,3 +1,24 @@
+// creates the accordion-style user input forms
+function initForms() {
+  buttons = document.getElementsByClassName("accordion");
+  for (i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", accordion());
+  }
+}
+
+//allows a button to accordion adjacent element
+function accordion(){
+  "use strict"
+  console.log(this);
+  this.classList.toggle("active");
+  var panel = this.nextElementSibling;
+  if (panel.style.display === "block") {
+    panel.style.display = "none";
+    console.log("hidden")
+  } else {
+    panel.style.display = "block";
+  }
+}
 
 function setColor(element, attrib, color) {
   var elems = document.getElementsByClassName(element);
