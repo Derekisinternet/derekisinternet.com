7485686a6707c3062405306ffb623c84212f5dcd
Derek <derekisinternet@gmail.com>
2021-01-28 18:57:00 -0800

    nicer code

--- js/videos.js
+++ js/videos.js
@@ -1,7 +1,8 @@
 // Select which video to play on the main player:
 function setVideo(link) {
-  document.getElementById('youtube-player').src = link;
+  var player = document.getElementById('youtube-player');
+  player.src = link;
   //redraw the player:
-  document.getElementById('youtube-player').parentElement.style.display = 'none';
-  document.getElementById('youtube-player').parentElement.style.display = 'block';
+  player.parentElement.style.display = 'none';
+  player.parentElement.style.display = 'block';
 }
