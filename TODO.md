a41df4b279f74188e0b4a6b9a88a3cd1234ca942
Derek <derekisinternet@gmail.com>
2021-01-30 18:59:21 -0800

    refactored to address multiple oscillators breaking playback. No sound comes out now, but at least it doesn't crash anymore

--- TODO.md
+++ TODO.md
@@ -13,11 +13,16 @@
   - [x] create oscillator
   - [x] create ui artifact that controls oscillator
   - [x] refactor power button to control local instance, rather than single global
-- [ ] control oscillator volume
-  - [ ] fix volume slider display
-  - [ ] update gain when slider changed
+- [x] control oscillator volume
+- [x] control oscillator waveform
+- [x] update gain when slider changed
+- [ ] BUG: creating multiple oscillators confuses each oscillator's toggle button
+  - I refactored the whole fucking mess into creatable JS objects, but now I'm running into undefineds
+  - turns out creating HTML elements in JS doesn't return a pointer to them. You gotta query the DOM for them I guess. Not a big deal, I can just start creating unique IDs for them and then using `document.getElementById()`
+  - still a bit complicated. I think it has to do with creating references in memory in different init methods that get destroyed outside the context. I think I just need:
+    - global hash with oscillator config
+    - html elements that lookup elements in global hash
 
-- [ ] 
 
 - [ ] create patch mechanism
 
--- demos/js/synth.js
+++ demos/js/synth.js
@@ -1,43 +1,75 @@
-var context;
+var context, oscCount, racks;
 
 function init(){
+  racks = {}; // memory to hold units
   context = new window.AudioContext();
+  oscCount = 0; // ensure each oscillator has a unique name
+
   start = document.createElement("input");
   start.type="button";
   start.value = "start";
-  start.onclick = function() {oscillatorFactory(context, "osc1");}
+  start.onclick = function() {
+    mod = oscillatorFactory(context, "osc" + oscCount);
+    racks[mod.name] = mod;
+  }
 
   document.getElementById("patchPanel").appendChild(start);
-  console.log("all ready");
 }
 
-// takes an audioContext and name, spits out an oscillator
-function oscillatorFactory(con, name) {
-  console.log('creating new oscillator');
-  // create oscillator and vol
-  o = con.createOscillator();
-  o.start(); // can only call start once, from then on, gotta dis/re-connect to toggle
-  g = con.createGain();
-  o.connect(g);
+// takes an audioContext and name, adds an oscillator to the ui
+function oscillatorFactory(ctx, name) {
+  // MODEL
+  oscillator = new OscMod(ctx, name);
+  console.log(oscillator);
+  parentDiv = document.getElementById('patchPanel');
+  // VIEW/CONTROLLER
+  ui = new OscUI(oscillator, parentDiv, oscillator.name);
+  return oscillator;
+}
 
-  // create ui elements to control osc
-  var m = moduleFactory(name);
-  var powerBtn = document.createElement("input");
+// takes an AudioContext and name
+function OscMod(ctx, name) {
+  this.name = name;
+  this.ctx = ctx;
+  this.osc = ctx.createOscillator();
+  this.gain = ctx.createGain();
+  this.osc.connect(this.gain);
+  this.osc.start();
+
+  // volume num has to be 0.0 >= n >= 1.0
+  this.setVolume = function(f) {
+    gain = racks[this.name].gain.gain;
+    console.log("gain:");
+    console.log(gain);
+    gain.setValueAtTime(f, this.ctx.currentTime);
+  }
+
+  this.setWave = function(type) {
+    this.osc.type = type;
+  }
+}
+
+// takes an OscMod object, a div to live in, and a unique identifier
+function OscUI(osc, div, name) {
+  this.oscMod = osc;
+  this.name = name;
+  // VIEW
+  this.parentDiv = div;
+
+  powerBtn = document.createElement("input");
+  powerBtn.id = "osc-"+this.name+'-pwr';
   powerBtn.type = "button";
   powerBtn.value = "on";
-  powerBtn.onclick = function() {powerSwitch(g, con, powerBtn);}
-
-  var volKnob = document.createElement("input");
+  
+  volKnob = document.createElement("input");
+  volKnob.id = "osc-"+this.name+'-vol';
   volKnob.type = "range";
   volKnob.min = "0.0";
   volKnob.max = '1.0';
   volKnob.step = '0.1';
-  function gainAdjust(node, val) {
-    node.gain.value = val;
-  }
-  volKnob.oninput = function() {gainAdjust(g, volKnob.value);}
-
-  var waveShaper = document.createElement("select");
+  
+  waveShaper = document.createElement("select");
+  waveShaper.id = "osc-"+this.name+'-wve';
   w = ["sine","square","sawtooth","triangle"];
   w.forEach( item => {
     e = document.createElement("option");
@@ -45,33 +77,30 @@ function oscillatorFactory(con, name) {
     e.innerHTML = item;
     waveShaper.appendChild(e);
   });
-  function setW(osc, val) {
-    osc.type = val;
+  // add to main view
+  parentDiv.appendChild(waveShaper);
+  parentDiv.appendChild(volKnob);
+  parentDiv.appendChild(powerBtn);
+
+  // CONTROLLERs
+  powerBtn.onclick = function() {
+    btn = document.getElementById(powerBtn.id);
+    unit = racks[name];
+    if (btn.value == "on") {
+      unit.setVolume(0);
+      btn.value = "off";
+    } else {
+      vol = document.getElementById(volKnob.id);
+      unit.setVolume(vol.value);
+      powerBtn.value = "on";
+    }
   }
-  waveShaper.onchange = function() {setW(o, waveShaper.value);}
 
-  m.appendChild(waveShaper);
-  m.appendChild(volKnob);
-  m.appendChild(powerBtn);
-
-  document.getElementById('patchPanel').appendChild(m);
-}
-
-// method for buttons to toggle audio
-// inp - input to control
-// ctx - audiocontext
-// btn - the button that calls this method
-function powerSwitch(inp, ctx, btn) {
-  ctx.resume();
-  if (btn.value == "on") {
-    console.log("turning on");
-    inp.connect(ctx.destination);
-    btn.value = "off";
-  } else {
-    console.log("turning off");
-    inp.disconnect(ctx.destination);
-    btn.value = "on";
+  waveShaper.onchange = function() {
+    this.oscMod.setWave(waveShaper.value);
   }
+
+  volKnob.oninput = function() {this.oscMod.setVolume(volKnob.value);}
 }
 
 // low-level helper that constructs a ui element
